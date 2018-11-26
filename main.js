50f9182970e664aec8669dbbfc41d4741c39c57d
Crisan Razvan <crsn_razvan@yahoo.com>
2018-11-26 19:34:48 +0200

    simplified functions

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
-function multiplyBy()
-{
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
+var firstNumber = document.getElementById("firstNumber");
+var secondNumber = document.getElementById("secondNumber");
+
+
+
+function multiplyBy() {
+        num1 = firstNumber.value;
+        num2 = secondNumber.value;
         document.getElementById("result").innerHTML = num1 * num2;
 }
 
-function divideBy() 
-{ 
-        num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
+function divideBy() {
+        num1 = firstNumber.value;
+        num2 = secondNumber.value;
         document.getElementById("result").innerHTML = num1 / num2;
 }
 
-function sumBy () {
-    num1 = document.getElementById("firstNumber").value;
-        num2 = document.getElementById("secondNumber").value;
+function sumBy() {
+        num1 = firstNumber.value;
+        num2 = secondNumber.value;
         document.getElementById("result").innerHTML = +num1 + +num2;
 }
 
-function diffBy () {
-    num1 = document.getElementById("firstNumber").value;
-    num2 = document.getElementById("secondNumber").value;
-    document.getElementById("result").innerHTML = num1 - num2;
+function diffBy() {
+        num1 = firstNumber.value;
+        num2 = secondNumber.value;
+        document.getElementById("result").innerHTML = num1 - num2;
 }
-
-
